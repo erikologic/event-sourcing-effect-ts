6eb3b220036c0c130b68388a7bf6467d329e2126
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-07-30 19:49:08 +0000

    refactored into types

--- src/functionalEventSourcing/types.ts
+++ src/functionalEventSourcing/types.ts
@@ -1,6 +1,6 @@
 // https://thinkbeforecoding.com/post/2021/12/17/functional-event-sourcing-decider
 
-import { number } from "effect/Equivalence"
+import type { Same, SelectFunctionByDiscriminantValueInParams } from "src/libs/utils"
 
 export interface IEvent {
   _type: "event"
@@ -29,5 +29,35 @@ export type IDecide<
   TEvent extends IEvent
 > = (command: TCommand, state: TState) => Array<TEvent>
 
-// TODO: fold
+export type FindEvolvers<
+  TEvolvers extends Array<IEvolve<any, any, any>>,
+  TState extends IState,
+  TEvent extends IEvent
+> = Same<
+  SelectFunctionByDiscriminantValueInParams<TEvolvers[number], "_tag", TState["_tag"]>,
+  SelectFunctionByDiscriminantValueInParams<TEvolvers[number], "_tag", TEvent["_tag"]>
+>
+
+export type Fold<
+  TState extends IState,
+  TEvents extends Array<IEvent>,
+  TEvolvers extends Array<IEvolve<any, any, any>>
+> = (
+  state: TState,
+  events: TEvents,
+  evolvers: TEvolvers
+) => TEvents extends
+  [infer CurrentEvent extends IEvent, infer NextEvent extends IEvent, ...infer RestEvents extends Array<IEvent>] ?
+  ReturnType<FindEvolvers<TEvolvers, TState, CurrentEvent>> extends infer ResultState extends IState ? ReturnType<
+      Fold<
+        ResultState,
+        [NextEvent, ...RestEvents],
+        TEvolvers
+      >
+    > :
+  never :
+  TEvents extends [infer CurrentEvent extends IEvent, ...infer _RestEvents extends Array<IEvent>] ?
+    ReturnType<FindEvolvers<TEvolvers, TState, CurrentEvent>> :
+  never
+
 // TODO: engine
--- src/orderManagement/index.ts
+++ src/orderManagement/index.ts
@@ -1,61 +1,16 @@
-import type { IEvent, IEvolve, IState } from "src/functionalEventSourcing/types"
-import type { Same, SelectFunctionByDiscriminantValueInParams } from "src/libs/utils"
-import type { AddItemToCartCommand } from "./commands"
-import type { Deciders } from "./deciders"
+import type { Fold } from "src/functionalEventSourcing/types"
 import type { BillingDetailsAddedEvent, ItemAddedEvent, PaymentCompletedEvent } from "./events"
 import type { Evolvers } from "./evolvers"
 import type { NewCartState } from "./states"
 
-type step1 = NewCartState
-type step2 = AddItemToCartCommand
-type step3 = Same<
-  SelectFunctionByDiscriminantValueInParams<Deciders, "_tag", step1["_tag"]>,
-  SelectFunctionByDiscriminantValueInParams<Deciders, "_tag", step2["_tag"]>
->
-type step4 = ReturnType<step3>[number]
-type step5 = Same<
-  SelectFunctionByDiscriminantValueInParams<Evolvers, "_tag", step1["_tag"]>,
-  SelectFunctionByDiscriminantValueInParams<Evolvers, "_tag", step4["_tag"]>
->
-type step6 = ReturnType<step5>
-
-type events = [ItemAddedEvent, BillingDetailsAddedEvent, PaymentCompletedEvent]
-type state0 = NewCartState
-type state1 = ReturnType<
-  Same<
-    SelectFunctionByDiscriminantValueInParams<Evolvers, "_tag", state0["_tag"]>,
-    SelectFunctionByDiscriminantValueInParams<Evolvers, "_tag", events[0]["_tag"]>
-  >
->
-type state2 = ReturnType<
-  Same<
-    SelectFunctionByDiscriminantValueInParams<Evolvers, "_tag", state1["_tag"]>,
-    SelectFunctionByDiscriminantValueInParams<Evolvers, "_tag", events[1]["_tag"]>
-  >
->
-type _state3 = ReturnType<
-  Same<
-    SelectFunctionByDiscriminantValueInParams<Evolvers, "_tag", state2["_tag"]>,
-    SelectFunctionByDiscriminantValueInParams<Evolvers, "_tag", events[2]["_tag"]>
-  >
->
-
-type FindEvolvers<TEvolvers extends Array<IEvolve<any, any, any>>, TState extends IState, TEvent extends IEvent> = Same<
-  SelectFunctionByDiscriminantValueInParams<TEvolvers[number], "_tag", TState["_tag"]>,
-  SelectFunctionByDiscriminantValueInParams<TEvolvers[number], "_tag", TEvent["_tag"]>
->
-
-type Fold<TState extends IState, TEvents extends Array<IEvent>, TEvolvers extends Array<IEvolve<any, any, any>>> = (
-  state: TState,
-  events: TEvents,
-  evolvers: TEvolvers
-) => TEvents extends [infer CurrentEvent extends IEvent, ...infer RestEvents] ? [
-    ReturnType<FindEvolvers<TEvolvers, TState, CurrentEvent>>,
-    RestEvents,
-    TEvolvers
-  ] :
-  never
-
-type _result = ReturnType<
+type _state1 = ReturnType<
   Fold<NewCartState, [ItemAddedEvent, BillingDetailsAddedEvent], Array<Evolvers>>
+>
+
+type _state2 = ReturnType<
+  Fold<NewCartState, [ItemAddedEvent, BillingDetailsAddedEvent, PaymentCompletedEvent], Array<Evolvers>>
+>
+
+type _stateInvalid = ReturnType<
+  Fold<NewCartState, [PaymentCompletedEvent], Array<Evolvers>>
 >
